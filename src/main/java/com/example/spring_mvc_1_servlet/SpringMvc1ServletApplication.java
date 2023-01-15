c368b8670da7221c1eb30de0e24e65fc8028c615
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-01-15 15:29:06 +0900

    servlet - 2

--- src/main/java/com/example/spring_mvc_1_servlet/SpringMvc1ServletApplication.java
+++ src/main/java/com/example/spring_mvc_1_servlet/SpringMvc1ServletApplication.java
@@ -2,7 +2,9 @@ package com.example.spring_mvc_1_servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class SpringMvc1ServletApplication {
 
