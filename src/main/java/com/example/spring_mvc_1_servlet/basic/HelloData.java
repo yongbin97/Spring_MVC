0410912ea3969a218bcd2e70bbc5cde74831d681
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-01-15 17:09:14 +0900

    servlet - JSON

---
+++ src/main/java/com/example/spring_mvc_1_servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_1_servlet.basic;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
