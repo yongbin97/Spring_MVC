3eb693591a08f9c6ac6aa48d32239527b0afb150
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-01-21 15:17:44 +0900

    Front controller 도입 - v1

---
+++ src/main/java/com/example/spring_mvc_1_servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,28 @@
+package com.example.spring_mvc_1_servlet.web.frontcontroller.v1.controller;
+
+import com.example.spring_mvc_1_servlet.domain.member.Member;
+import com.example.spring_mvc_1_servlet.domain.member.MemberRepository;
+import com.example.spring_mvc_1_servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/com/example/spring_mvc_1_servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,34 @@
+package com.example.spring_mvc_1_servlet.web.frontcontroller.v1.controller;
+
+import com.example.spring_mvc_1_servlet.domain.member.Member;
+import com.example.spring_mvc_1_servlet.domain.member.MemberRepository;
+import com.example.spring_mvc_1_servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //model에 데이터 보관
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
