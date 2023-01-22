ca2340ecb8751463912e773c1874032270bd9e3a
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-01-22 23:26:41 +0900

    spring mvc - logging

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// RestController: 반환 값을 그대로 HTTP Message body에 넣어서 response
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+
+    }
+}
